87e0fa2d1844f4a811d82bee9e50d451505762d6
yourname <your@email.com>
2022-05-01 10:26:42 +0900

    added messages

--- yourfeelingnow.js
+++ yourfeelingnow.js
@@ -1,3 +1,4 @@
+/* dataからパラメータ取得　連想配列を想定*/
 param = {
     "sweetness": 5000, 
     "astringency": 3, 
@@ -12,6 +13,7 @@ classchange = {
     "bitterness": "にがい",
     "sour": "すっぱい"
             }
+/*paramの値だけすべて習得、最大値を探してその値に紐づいたキーを返す*/ 
 param_value = Object.values(param)
 var max = param_value.reduce(function(a, b) {
     return Math.max(a, b);
@@ -20,9 +22,11 @@ var maxkey = Object.keys(param).filter( (key) => {
   return param[key] === max
  });
 console.log(maxkey)
+/*htmlのlargestタグの部分にキーに基づいた表示文を表示*/ 
 document.getElementById('largest').innerHTML = classchange.maxkey
 console.log(document.getElementById('largest').innerHTML)
 var keyword = document.getElementById("search")
 var value = keyword.value
+/*ユーザーが入れたキーワードをkeywordタグの部分にそのまま表示*/ 
 document.getElementById('keyword').innerHTML = keyword.value
 console.log(document.getElementById('keyword').innerHTML)
